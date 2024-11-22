68f8d1f428fe04d10d442bb9e7a98c2b8be1f227
Greg Bluwell <contact@gregbluwell.dev>
2024-11-22 11:17:49 +0100

    ADD Tout les Composants.vue + Script Logique basiques

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script setup>
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+import { ref } from 'vue';
 
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: +299.97 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+]);
 
 </script>
 
 <template>
-  
-  Hello i'm APP component
+    <Header />
+
+    <div class="container">
+      <Balance />
+      <IncomeExpense />
+      <TransactionList :transactions= "transactions"/>
+      <AddTransaction />
+    </div>
 
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
